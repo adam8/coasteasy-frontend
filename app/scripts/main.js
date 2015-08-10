73bea450deb830b8eb18deda5e87e9e1b976ddd1
Adam Jones <adam@adam8.com>
2015-08-09 19:35:27 -0500

    packery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -214,9 +214,9 @@ var Posts = React.createClass({displayName: "Posts",
         rows.push(React.createElement(PostItem, {post: post, handleVoteUp: this.props.handleVoteUp, key:  post.id}));
         //lastDate = day;
       }.bind(this));
-      return (React.createElement("div", {className: "posts-list"},  rows ));
+      return (React.createElement("div", {id: "posts", className: "posts-list"},  rows ));
     } else {
-      return (React.createElement("div", {className: "posts-list"}, "No posts found."));
+      return (React.createElement("div", {id: "posts", className: "posts-list"}, "No posts found."));
     }
   }
 });
@@ -229,7 +229,7 @@ var Posts = React.createClass({displayName: "Posts",
     );
   }
 });
-*/}
+*/} 
 
 var PostItem = React.createClass({displayName: "PostItem",
   mixins: [Router.State, Navigation],
@@ -520,7 +520,21 @@ var App = React.createClass({displayName: "App",
       React.createElement("div", {id: "content-container", className:  "page-" + this.state.currentPage + " login-" + this.state.isLogin}, 
 
         React.createElement("div", {id: "header", onClick: this.handleHeaderClick}, 
-          React.createElement("div", {id: "logo"}, React.createElement("h1", null, React.createElement("a", {href: "/", onClick: this.handleGoHome, onTouchStart: this.handleGoHome}, "Coast Easy"))), 
+          React.createElement("div", {id: "logo"}, 
+            React.createElement("h1", null, 
+              React.createElement("a", {href: "/", onClick: this.handleGoHome, onTouchStart: this.handleGoHome}, 
+                React.createElement("svg", {version: "1.1", id: "logo-svg", xmlns: "http://www.w3.org/2000/svg", x: "0px", y: "0px", 
+                	 viewBox: "0 0 177.7 177.9", "enable-background": "new 0 0 177.7 177.9"}, 
+                  React.createElement("path", {id: "XMLID_9_", opacity: "0.89", fill: "#1C7499", d: "M36.9,84.3c2.4-26.6,24.8-47.5,52-47.5s49.6,20.9,52,47.5h36.9" + ' ' +
+                  	"C175.3,37.3,136.4,0,88.9,0S2.4,37.3,0,84.3H36.9z"}), 
+                  React.createElement("path", {id: "XMLID_10_", opacity: "0.89", fill: "#1C7499", d: "M48.5,94.3c2.6,20,19.7,35.4,40.4,35.4c5.6,0,10.9-1.1,15.8-3.2" + ' ' +
+                  	"c1.7,3.1,3.4,6.2,5.2,9.2c0.2,0.3,0.3,0.5,0.5,0.7c-6.6,3-13.8,4.7-21.5,4.7c-27,0-49.3-20.6-51.9-46.9H0" + ' ' +
+                  	"c2.8,46.7,41.5,83.6,88.8,83.6s86.1-37,88.8-83.6H48.5z"}), 
+                  React.createElement("path", {id: "XMLID_13_", opacity: "0.71", fill: "#FFCF05", d: "M88.8,48.2C68,48.2,50.7,64,48.4,84.3h80.9C127,64,109.7,48.2,88.8,48.2z"})
+                )
+              )
+            )
+          ), 
            this.state.currentPage !== "new-post" ? React.createElement(AddPostButton, {handleNewPost: this.handleNewPost}) : null
           /* this.state.isLogin === true ? <LogoutButton handleDoLogout={this.handleDoLogout} user={this.state.user} /> : <LoginButton currentPage={this.state.currentPage} handleDoLogin={this.handleDoLogin} /> */
         ), 
@@ -553,3 +567,9 @@ var routes = (
 Router.run(routes, function (Handler, state) {
   React.render(React.createElement(Handler, null), document.getElementById('app'));
 });
+
+
+
+
+
+
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -214,9 +214,9 @@ var Posts = React.createClass({
         rows.push(<PostItem post={post} handleVoteUp={this.props.handleVoteUp} key={ post.id } />);
         //lastDate = day;
       }.bind(this));
-      return (<div className="posts-list">{ rows }</div>);
+      return (<div id="posts" className="posts-list">{ rows }</div>);
     } else {
-      return (<div className="posts-list">No posts found.</div>);
+      return (<div id="posts" className="posts-list">No posts found.</div>);
     }
   }
 });
@@ -229,7 +229,7 @@ var Posts = React.createClass({
     );
   }
 });
-*/}
+*/} 
 
 var PostItem = React.createClass({
   mixins: [Router.State, Navigation],
@@ -520,7 +520,21 @@ var App = React.createClass({
       <div id="content-container" className={ "page-" + this.state.currentPage + " login-" + this.state.isLogin }>
 
         <div id="header" onClick={this.handleHeaderClick}>
-          <div id="logo"><h1><a href="/" onClick={this.handleGoHome} onTouchStart={this.handleGoHome}>Coast Easy</a></h1></div>
+          <div id="logo">
+            <h1>
+              <a href="/" onClick={this.handleGoHome} onTouchStart={this.handleGoHome}>  
+                <svg version="1.1" id="logo-svg" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                	 viewBox="0 0 177.7 177.9" enable-background="new 0 0 177.7 177.9">
+                  <path id="XMLID_9_" opacity="0.89" fill="#1C7499" d="M36.9,84.3c2.4-26.6,24.8-47.5,52-47.5s49.6,20.9,52,47.5h36.9
+                  	C175.3,37.3,136.4,0,88.9,0S2.4,37.3,0,84.3H36.9z"/>
+                  <path id="XMLID_10_" opacity="0.89" fill="#1C7499" d="M48.5,94.3c2.6,20,19.7,35.4,40.4,35.4c5.6,0,10.9-1.1,15.8-3.2
+                  	c1.7,3.1,3.4,6.2,5.2,9.2c0.2,0.3,0.3,0.5,0.5,0.7c-6.6,3-13.8,4.7-21.5,4.7c-27,0-49.3-20.6-51.9-46.9H0
+                  	c2.8,46.7,41.5,83.6,88.8,83.6s86.1-37,88.8-83.6H48.5z"/>
+                  <path id="XMLID_13_" opacity="0.71" fill="#FFCF05" d="M88.8,48.2C68,48.2,50.7,64,48.4,84.3h80.9C127,64,109.7,48.2,88.8,48.2z"/>
+                </svg>
+              </a>
+            </h1>
+          </div>
           { this.state.currentPage !== "new-post" ? <AddPostButton handleNewPost={this.handleNewPost} /> : null }
           {/* this.state.isLogin === true ? <LogoutButton handleDoLogout={this.handleDoLogout} user={this.state.user} /> : <LoginButton currentPage={this.state.currentPage} handleDoLogin={this.handleDoLogin} /> */}
         </div>
@@ -553,3 +567,9 @@ var routes = (
 Router.run(routes, function (Handler, state) {
   React.render(<Handler />, document.getElementById('app'));
 });
+
+
+
+
+
+
